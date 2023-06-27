2a8d0aef2c74481a7a8687aa908daa5f0e0bf50c
Luke Rhoads <pumpkineater661@gmail.com>
2023-06-27 18:23:53 -0500

    Added database interaction

---
+++ db_tester.py
@@ -0,0 +1,13 @@
+from firebase_admin import credentials, firestore, initialize_app
+from db import Collection, DatabaseInteractor
+
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)
+db = firestore.client()
+db_interactor = DatabaseInteractor(db)
+
+# Random test code
+student_id = db_interactor.create_student(schoolId=0, name="HPHS", email="hello@hello.com")
+club_id = db_interactor.create_club(name=0, studentId=student_id, tags=["hello", "there"])
+student_2_id = db_interactor.create_student(schoolId=0, name="HPHS", email="hello2@hello.com")
+db_interactor.add_student_to_club(studentId=student_2_id, clubId=club_id)
