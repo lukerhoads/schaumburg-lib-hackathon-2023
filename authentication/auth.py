aa430f14c999756cb819f2786a3e80258ec61f25
Audric Serador <audricciel@gmail.com>
2023-06-27 16:33:13 -0500

    more setup

--- authentication/auth.py
+++ authentication/auth.py
@@ -5,3 +5,7 @@ auth = Blueprint("auth", __name__, static_folder="static", template_folder="temp
 @auth.route("/login")
 def login():
     return render_template("login.html")
+
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
