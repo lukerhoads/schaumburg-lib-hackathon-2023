5dab4edd96a65b54f63d0523d7171c4b2561789c
Audric Serador <audricciel@gmail.com>
2023-06-27 15:51:01 -0500

    delete duplicate folder

--- auth/auth.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-auth = Blueprint("auth", __name__, static_folder="static", template_folder="templates")
-
-@auth.route("/login")
-def login():
-    return render_template("login.html")
