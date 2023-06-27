8ffb480f264321851900d5e45c7edec323d82818
Audric Serador <audricciel@gmail.com>
2023-06-27 17:25:22 -0500

    creating signup pages

--- authentication/auth.py
+++ authentication/auth.py
@@ -6,6 +6,18 @@ auth = Blueprint("auth", __name__, static_folder="static", template_folder="temp
 def login():
     return render_template("login.html")
 
-@auth.route("/signup")
+@auth.route("/sign-up")
 def signup():
     return render_template("signup.html")
+
+@auth.route("/sign-up/student")
+def signupStudent():
+    return render_template("signup_student.html")
+
+@auth.route('/sign-up/sponsor')
+def signupSponsor():
+    return render_template("signup_sponsor.html")
+
+@auth.route('sign-up/admin')
+def signupAdmin():
+    return render_template("signup_admin.html")
